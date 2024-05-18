889cb98d44098681ec326a0ec910d1c400f68b89
Artsiom Shushkevich <artsiom_shushkevich@epam.com>
2024-05-18 11:26:58 +0300

    Add manual base path setup

--- next.config.js
+++ next.config.js
@@ -1,10 +1,23 @@
 // @ts-check
 
+const { PHASE_PRODUCTION_BUILD } = require('next/constants');
+
+const BASE_PATH = '/blog';
+
 module.exports = async phase => {
     /**
      * @type {import('next').NextConfig}
      */
-    const nextConfig = {};
+    const nextConfig = {
+        ...(phase === PHASE_PRODUCTION_BUILD
+            ? {
+                  basePath: BASE_PATH,
+                  images: {
+                      path: `${BASE_PATH}/_next/image`
+                  }
+              }
+            : {})
+    };
 
     return nextConfig;
 };
