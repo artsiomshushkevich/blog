1f71a3f70da6c7b3c7a6b523a637befb174e75ea
Artsiom Shushkevich <artsiom_shushkevich@epam.com>
2024-05-18 11:57:55 +0300

    Add back default config

--- next.config.js
+++ next.config.js
@@ -4,29 +4,34 @@
 
 // const BASE_PATH = '/blog';
 
-module.exports = async phase => {
-    /**
-     * @type {import('next').NextConfig}
-     */
-    const nextConfig = {
-        // ...(phase === PHASE_PRODUCTION_BUILD
-        //     ? {
-        //           basePath: BASE_PATH,
-        //           images: {
-        //               remotePatterns: [
-        //                   {
-        //                       protocol: 'https',
-        //                       hostname: 'artsiomshushkevich.github.io',
-        //                       port: '',
-        //                       pathname: '/blog/**'
-        //                   }
-        //               ],
-        //               path: `${BASE_PATH}/_next/image`,
-        //               unoptimized: true
-        //           }
-        //       }
-        //     : {})
-    };
+// module.exports = async phase => {
+//     /**
+//      * @type {import('next').NextConfig}
+//      */
+//     const nextConfig = {
+//         // ...(phase === PHASE_PRODUCTION_BUILD
+//         //     ? {
+//         //           basePath: BASE_PATH,
+//         //           images: {
+//         //               remotePatterns: [
+//         //                   {
+//         //                       protocol: 'https',
+//         //                       hostname: 'artsiomshushkevich.github.io',
+//         //                       port: '',
+//         //                       pathname: '/blog/**'
+//         //                   }
+//         //               ],
+//         //               path: `${BASE_PATH}/_next/image`,
+//         //               unoptimized: true
+//         //           }
+//         //       }
+//         //     : {})
+//     };
 
-    return nextConfig;
-};
+//     return nextConfig;
+// };
+
+/**
+ * @type {import('next').NextConfig}
+ */
+exports.default = {};
