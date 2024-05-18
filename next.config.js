1507e5d1539303790b8adba21a3479393345cc31
Artsiom Shushkevich <artsiom_shushkevich@epam.com>
2024-05-18 11:55:14 +0300

    Test without config changes

--- next.config.js
+++ next.config.js
@@ -1,31 +1,31 @@
 // @ts-check
 
-const { PHASE_PRODUCTION_BUILD } = require('next/constants');
+// const { PHASE_PRODUCTION_BUILD } = require('next/constants');
 
-const BASE_PATH = '/blog';
+// const BASE_PATH = '/blog';
 
 module.exports = async phase => {
     /**
      * @type {import('next').NextConfig}
      */
     const nextConfig = {
-        ...(phase === PHASE_PRODUCTION_BUILD
-            ? {
-                  basePath: BASE_PATH,
-                  images: {
-                      remotePatterns: [
-                          {
-                              protocol: 'https',
-                              hostname: 'artsiomshushkevich.github.io',
-                              port: '',
-                              pathname: '/blog/**'
-                          }
-                      ],
-                      path: `${BASE_PATH}/_next/image`,
-                      unoptimized: true
-                  }
-              }
-            : {})
+        // ...(phase === PHASE_PRODUCTION_BUILD
+        //     ? {
+        //           basePath: BASE_PATH,
+        //           images: {
+        //               remotePatterns: [
+        //                   {
+        //                       protocol: 'https',
+        //                       hostname: 'artsiomshushkevich.github.io',
+        //                       port: '',
+        //                       pathname: '/blog/**'
+        //                   }
+        //               ],
+        //               path: `${BASE_PATH}/_next/image`,
+        //               unoptimized: true
+        //           }
+        //       }
+        //     : {})
     };
 
     return nextConfig;
