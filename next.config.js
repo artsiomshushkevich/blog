93afe312b4cf350227deb0f0c85f79576cb23687
Artsiom Shushkevich <artsiom_shushkevich@epam.com>
2024-05-18 12:09:03 +0300

    Test config as callback

--- next.config.js
+++ next.config.js
@@ -4,34 +4,34 @@
 
 // const BASE_PATH = '/blog';
 
-// module.exports = async phase => {
-//     /**
-//      * @type {import('next').NextConfig}
-//      */
-//     const nextConfig = {
-//         // ...(phase === PHASE_PRODUCTION_BUILD
-//         //     ? {
-//         //           basePath: BASE_PATH,
-//         //           images: {
-//         //               remotePatterns: [
-//         //                   {
-//         //                       protocol: 'https',
-//         //                       hostname: 'artsiomshushkevich.github.io',
-//         //                       port: '',
-//         //                       pathname: '/blog/**'
-//         //                   }
-//         //               ],
-//         //               path: `${BASE_PATH}/_next/image`,
-//         //               unoptimized: true
-//         //           }
-//         //       }
-//         //     : {})
-//     };
+module.exports = async phase => {
+    /**
+     * @type {import('next').NextConfig}
+     */
+    const nextConfig = {
+        // ...(phase === PHASE_PRODUCTION_BUILD
+        //     ? {
+        //           basePath: BASE_PATH,
+        //           images: {
+        //               remotePatterns: [
+        //                   {
+        //                       protocol: 'https',
+        //                       hostname: 'artsiomshushkevich.github.io',
+        //                       port: '',
+        //                       pathname: '/blog/**'
+        //                   }
+        //               ],
+        //               path: `${BASE_PATH}/_next/image`,
+        //               unoptimized: true
+        //           }
+        //       }
+        //     : {})
+    };
 
-//     return nextConfig;
-// };
+    return {};
+};
 
-/**
- * @type {import('next').NextConfig}
- */
-module.exports = {};
+// /**
+//  * @type {import('next').NextConfig}
+//  */
+// module.exports = {};
